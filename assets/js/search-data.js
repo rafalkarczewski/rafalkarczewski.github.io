03f6d25fed0ecd42e6300758cd4f5b8b3e4edbbf
rafalkarczewski <karczewski.rafal93@gmail.com>
2025-06-11 17:09:49 +0000

    Deploying to gh-pages from @ rafalkarczewski/rafalkarczewski.github.io@a619bb7ee0334e26e14e46fdf8478cd25aa0b49b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,6 +423,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/rafalkarczewski", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
